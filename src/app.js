51dd435ac52b7c8d930dd1c0ff36ad6658472778
Samagun <67420501+Samagun@users.noreply.github.com>
2020-10-20 17:19:03 +0600

    add component

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HeroSection from "./components/hero-section-begin/hero-section";
+
+const App = () => {
+    return(
+        <div>
+            <HeroSection/>
+        </div>
+    )
+}
+export default App;
---
+++ src/components/hero-categories/hero-categories-all.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const HeroCategories = () => {
+    return(
+        <div></div>
+    )
+}
+export default HeroCategories;
